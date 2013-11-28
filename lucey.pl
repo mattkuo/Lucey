371d346ac099c02a6dc52f129ef89732eb45bd07
Tricia Jose <triciajose@gmail.com>
2013-11-27 23:59:36 -0800

    documentation

--- lucey.pl
+++ lucey.pl
@@ -1,3 +1,71 @@
+%% LUCEY is a Clue Board Game Assistant
+% 			created by Tzyy-Shiuan (Matthew) Kuo - 50387109 - k6y7
+% 			and Tricia Jose - 47742101 - b4b8 
+
+%% TO START THE PROGRAM
+% (when using the program, please do not enter any quotation marks, 
+%	they are in this instruction manual simply for ease of reading)
+
+% - compile program by entering "[lucey]."
+% - start the program by entering "clue."
+% - you will then be prompted to enter necessary information for starting the game
+
+%% A FEW NOTES
+
+% - all commands in this program must be terminated with a "."
+% - this program assumes that game pieces are the standard/default Clue board game pieces
+
+%% HOW THE PROGRAM WORKS
+
+% THE MAIN MENU
+
+% The user is presented with 5 options after all the necessary start-up information has been input.
+% Each time the menu is instantiated, it also checks whether or not a final accusation can be made,
+%	that is, when only one item from each category remains unknown
+% The menu options are as follows:
+
+
+% 1. Record my suggestion
+
+%		- if a card is shown to me by player X, it is added to my database in two areas:
+%			a) my list of known cards
+%			b) the list of cards player X has in his hand
+%		- if a card is not shown to me, and i have the remaining ones in my hand,
+%			all the other cards of that category are moved to my list of known cards
+
+% 2. Record my opponent's suggestion
+
+%		- if a card is shown by me to player X, it is recorded in my database as a card that player X has seen that I have shown
+% 		- if a card is shown by another player Y to player X, it is added to my database in one of 2 ways:
+%			1) 	if i have or know of the remaining cards:
+%					a. my list of known cards
+%					b. the list of cards player Y has
+%			2) 	else:
+%					a. the list of cards player X may have seen
+%					b. the list of cards player Y may have
+%		- if no card is shown to person X, and either i have the remaining ones in my hand, or person X does,
+%			all the other cards of that category are moved to my list of known cards
+
+% 3. Suggest next move
+
+%		- makes a suggestion based on what room you're in, will use two known cards to learn something about an unknown card
+
+% 4. View Database
+%		- This displays the entire database and is split into sections:
+%		- a. Cards that remain unknown to me, divided into suspect/weapon/room
+%		- b. Cards that cannot be in the envelope (i hold or have seen them or have deduced them)
+%		- c. Knowledge about my opponents cards
+%					each card is assigned a "status" in this section:
+% 						% Status is a number (0, 1, 2, 3, 4):  
+%				 		0 - Player does not have card in their hand.
+%				 		1 - Player might have card in their hand.
+%				 		2 - Player has card in their hand.
+%				 		3 - This is my card and player has seen it
+%				 		4 - This may be someone else's card and player may have seen this card
+
+% 5. Exit program: this exits the program
+
+
 clue :- get_startup_data, main_menu.
 
 % Dynamic database
